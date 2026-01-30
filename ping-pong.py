923e536636e222de64654a5a392057c624ec5c88
VitaliiFirsovv2011 <firsovv2011@gmail.com>
2026-01-30 21:12:58 +0400

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,27 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, image_file, x, y, speed, size_x, size_y):
+        super().__init__()
+        self.image = transform.scale(image.load(image_file), (size_x, size_y))
+        self.speed = speed
+        self.rect  = (self.image.get_rect())
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
@@ -8,12 +30,21 @@ window.fill(back)
 
 clock = time.Clock()
 FPS = 60
+
 game = True
+finish = False
+
+player1 = Player('Pygame/Картинки/racket.png', 30, 200, 4, 50, 150)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    if finish != True:
+        window.fill(back)
+        player1.update_l()
+        player1.reset()
 
     display.update()
     clock.tick(FPS)
